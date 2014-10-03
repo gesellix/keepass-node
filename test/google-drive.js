07b86f51269c2d6468a9ecbdb953aa76bcbcb638
Tobias Gesellchen <tobias@gesellix.de>
2014-10-03 16:51:57 +0200

    add even more tests

--- test/google-drive.js
+++ test/google-drive.js
@@ -7,9 +7,8 @@ chai.use(chaiAsPromised);
 var should = chai.should();
 
 var googleDriveConfig = {
-  "enabled": false,
   "clientSecret": {
-    "client_id": "123456789012-abcdefghijklmnopqrstuvwxyz123456.apps.googleusercontent.com",
+    "client_id": "client-id-0.apps.googleusercontent.com",
     "client_secret": "aBcDeFgHiJkL987456_01234",
     "redirect_uris": ["https://www.example.com:8843/update/oauth2callback"]
   },
@@ -76,7 +75,7 @@ describe('GET /', function () {
       request(app)
           .get('/test-path')
           .set('Accept', 'application/json')
-          .expect("Location", "https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.readonly&response_type=code&client_id=123456789012-abcdefghijklmnopqrstuvwxyz123456.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwww.example.com%3A8843%2Fupdate%2Foauth2callback")
+          .expect("Location", "https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.readonly&response_type=code&client_id=client-id-0.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwww.example.com%3A8843%2Fupdate%2Foauth2callback")
           .expect(302, done);
     });
   });
@@ -84,10 +83,9 @@ describe('GET /', function () {
   describe('with empty oauth2Tokens', function () {
     it('should redirect to auth refresh uri', function (done) {
       var googleDriveConfig = {
-        "enabled": false,
         "oauth2Tokens": {},
         "clientSecret": {
-          "client_id": "123456789012-abcdefghijklmnopqrstuvwxyz123456.apps.googleusercontent.com",
+          "client_id": "client-id-1.apps.googleusercontent.com",
           "client_secret": "aBcDeFgHiJkL987456_01234",
           "redirect_uris": ["https://www.example.com:8843/update/oauth2callback"]
         },
@@ -101,8 +99,31 @@ describe('GET /', function () {
       request(app)
           .get('/test-path')
           .set('Accept', 'application/json')
-          .expect("Location", "https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.readonly&response_type=code&client_id=123456789012-abcdefghijklmnopqrstuvwxyz123456.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwww.example.com%3A8843%2Fupdate%2Foauth2callback")
+          .expect("Location", "https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.readonly&response_type=code&client_id=client-id-1.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwww.example.com%3A8843%2Fupdate%2Foauth2callback")
           .expect(302, done);
+    });
+  });
+
+  describe('with empty clientSecret', function () {
+    describe('and existing clientSecretFilename', function () {
+      it('should set clientSecret clientSecretFile contents', function (done) {
+        var googleDriveConfig = {
+          "oauth2Tokens": {},
+          clientSecretFilename: __dirname + '/resources/googleapis_client_secret.json',
+          clientSecretType: 'web',
+          "fileTitle": 'your_keepass_db_filename_in_google_drive.kdbx'
+        };
+
+        var app = express();
+        var middleware = googleTestDrive(googleDriveConfig);
+        app.use('/test-path', middleware);
+
+        request(app)
+            .get('/test-path')
+            .set('Accept', 'application/json')
+            .expect("Location", "https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.readonly&response_type=code&client_id=client-id-2.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwww.example.com%3A8843%2Fupdate%2Foauth2callback")
+            .expect(302, done);
+      });
     });
   });
 });
