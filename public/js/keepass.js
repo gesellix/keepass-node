0720b2d7d5e1cb2fa342943f0d65bf74145e8cb2
Tobias Gesellchen <tobias@gesellix.de>
2014-10-07 23:27:09 +0200

    authorize via JWT

--- public/js/keepass.js
+++ public/js/keepass.js
@@ -2,33 +2,92 @@
 
 var keepass = angular.module('keepass', ['init', 'angularTreeview', 'keepass-entries']);
 
+keepass.provider('jwtInterceptor2', function () {
+
+  this.authHeader = 'Authorization';
+  this.authPrefix = 'Bearer ';
+  this.tokenGetter = function () {
+    return null;
+  };
+
+  var config = this;
+
+  this.$get = function ($q, $injector, $rootScope) {
+    return {
+      request: function (request) {
+        if (request.skipAuthorization) {
+          return request;
+        }
+
+        request.headers = request.headers || {};
+        // Already has an Authorization header
+        if (request.headers[config.authHeader]) {
+          return request;
+        }
+
+        var tokenPromise = $q.when($injector.invoke(config.tokenGetter, this, {
+          config: request
+        }));
+
+        return tokenPromise.then(function (token) {
+          if (token) {
+            request.headers[config.authHeader] = config.authPrefix + token;
+          }
+          return request;
+        });
+      },
+      responseError: function (response) {
+        // handle the case where the user is not authenticated
+        if (response.status === 401) {
+          $rootScope.$broadcast('unauthenticated', response);
+        }
+        return $q.reject(response);
+      }
+    };
+  };
+});
+
+keepass.config(function ($httpProvider, jwtInterceptor2Provider) {
+  jwtInterceptor2Provider.tokenGetter = function () {
+    return localStorage.getItem('jwt');
+  };
+  $httpProvider.interceptors.push('jwtInterceptor2');
+});
+
 keepass.service('kdbxBackendService', function ($http) {
   this.getDatabases = function () {
     return $http({
-                   "method": "get",
-                   "url": '/databases'
-                 });
+      "method": "get",
+      "url": '/databases'
+    });
+  };
+  this.getDatabaseAuthToken = function (filename, password) {
+    return $http({
+      "method": "post",
+      "url": '/databases/' + encodeURIComponent(filename) + '/auth',
+      data: {password: password}
+    });
   };
   this.getRaw = function (filename, password) {
     return $http({
-                   "method": "post",
-                   "url": '/databases/' + encodeURIComponent(filename),
-                   data: {password: password}
-                 });
+      "method": "post",
+      "url": '/databases/' + encodeURIComponent(filename),
+      data: {password: password}
+    });
   };
   this.getGroups = function (filename, password) {
     return $http({
-                   "method": "post",
-                   "url": '/' + encodeURIComponent(filename) + '/groups',
-                   data: {password: password}
-                 });
+      "method": "post",
+      "url": '/' + encodeURIComponent(filename) + '/groups',
+      data: {password: password}
+    });
   };
   this.getEntries = function (filename, password, group) {
     return $http({
-                   "method": "post",
-                   "url": '/' + encodeURIComponent(filename) + '/' + encodeURIComponent(group),
-                   data: {password: password}
-                 });
+      "method": "post",
+      "url": '/' + encodeURIComponent(filename) + '/' + encodeURIComponent(group),
+      data: {password: password}
+    });
   };
 });
 
@@ -56,6 +115,16 @@ keepass.controller('keepassBrowser', function ($scope, init, kdbxBackendService)
 
   $scope.loadEntries = function () {
     $scope.errors = [];
+    $scope.messages = ["authenticate..."];
+    kdbxBackendService.getDatabaseAuthToken($scope.selectedDb, $scope.dbPassword)
+        .then(function (result) {
+          console.log(result.data);
+          localStorage.setItem('jwt', result.data.jwt);
+        }, function (reason) {
+          console.log(reason);
+        });
+
+    $scope.errors = [];
     $scope.messages = ["loading..."];
     $scope.groupsTree = [];
     $scope.groupEntries = [];
@@ -65,17 +134,17 @@ keepass.controller('keepassBrowser', function ($scope, init, kdbxBackendService)
     //          });
     kdbxBackendService.getGroups($scope.selectedDb, $scope.dbPassword)
         .then(function (result) {
-                $scope.errors = [];
-                $scope.messages = [];
-                $scope.messages.push("groups successfully loaded");
-                onGroupsLoaded(result.data);
-              },
-              function (reason) {
-                $scope.messages = [];
-                $scope.errors = [];
-                $scope.errors.push("load groups HTTP status: " + reason.status);
-                $scope.errors.push(reason.data);
-              });
+          $scope.errors = [];
+          $scope.messages = [];
+          $scope.messages.push("groups successfully loaded");
+          onGroupsLoaded(result.data);
+        },
+        function (reason) {
+          $scope.messages = [];
+          $scope.errors = [];
+          $scope.errors.push("load groups HTTP status: " + reason.status);
+          $scope.errors.push(reason.data);
+        });
   };
 
   init('keepassBrowser', [kdbxBackendService.getDatabases()], function (result) {
@@ -93,17 +162,17 @@ keepass.controller('keepassBrowser', function ($scope, init, kdbxBackendService)
       $scope.groupEntries = [];
       kdbxBackendService.getEntries($scope.selectedDb, $scope.dbPassword, $scope.kdbxTree.currentNode.UUID)
           .then(function (result) {
-                  $scope.errors = [];
-                  $scope.messages = [];
-                  $scope.messages.push("entries successfully loaded");
-                  onGroupSelected(result.data);
-                },
-                function (reason) {
-                  $scope.messages = [];
-                  $scope.errors = [];
-                  $scope.errors.push("load entries HTTP status: " + reason.status);
-                  $scope.errors.push(reason.data);
-                });
+            $scope.errors = [];
+            $scope.messages = [];
+            $scope.messages.push("entries successfully loaded");
+            onGroupSelected(result.data);
+          },
+          function (reason) {
+            $scope.messages = [];
+            $scope.errors = [];
+            $scope.errors.push("load entries HTTP status: " + reason.status);
+            $scope.errors.push(reason.data);
+          });
     }
   }, false)
 });
--- server.js
+++ server.js
@@ -6,6 +6,8 @@
   var keepass = keepassLib.Keepass(__dirname + '/local/');
 
   var express = require('express');
+  var expressJwt = require('express-jwt');
+  var jwt = require('jsonwebtoken');
 
   var app = express();
 
@@ -38,41 +40,65 @@
     keepass.getDatabaseNames().then(function (result) {
       res.json(result);
     }, function (reason) {
-      res.send("error reading database list: " + reason, 500);
+      res.status(500).send("error reading database list: " + reason);
     });
   });
   app.post('/databases/:filename', function (req, res) {
     var databaseName = req.params.filename;
     if (!keepass.exists(databaseName)) {
-      res.send("database '" + databaseName + "' doesn't exist", 404);
+      res.status(404).send("database '" + databaseName + "' doesn't exist");
     }
     else {
       var password = req.body.password;
       keepass.getDatabaseRaw(databaseName, password).then(function (result) {
         res.json(result);
       }, function (reason) {
-        res.send("problem occurred reading '" + databaseName + "': " + reason, 500);
+        res.status(500).send("problem occurred reading '" + databaseName + "': " + reason);
       });
     }
   });
-  app.post('/:filename/groups', function (req, res) {
-    var databaseName = req.params.filename;
-    if (!keepass.exists(databaseName)) {
-      res.send("database '" + databaseName + "' doesn't exist", 404);
-    }
-    else {
-      var password = req.body.password;
-      keepass.getDatabaseGroups(databaseName, password).then(function (result) {
-        res.json(result);
-      }, function (reason) {
-        res.send("problem occurred reading '" + databaseName + "': " + reason, 500);
+  // TODO. obviously.
+  var secret = 'shhhhhhared-secret';
+  app.post('/databases/:filename/auth',
+      function (req, res) {
+        var databaseName = req.params.filename;
+        if (!keepass.exists(databaseName)) {
+          res.status(404).send("database '" + databaseName + "' doesn't exist");
+        }
+        else {
+          var password = req.body.password;
+          keepass.getDatabaseRaw(databaseName, password).then(function (result) {
+            var token = jwt.sign({filename: databaseName, password: password}, secret);
+            res.json({jwt: token});
+          }, function (reason) {
+            res.status(500).send("problem occurred reading '" + databaseName + "': " + reason);
+          });
+        }
       });
-    }
-  });
+  app.post('/:filename/groups',
+      expressJwt({secret: secret, userProperty: 'jwt'}),
+      function (req, res) {
+        if (req.jwt.filename != req.params.filename) {
+          res.status(401).send("access denied");
+          return;
+        }
+        var databaseName = req.params.filename;
+        if (!keepass.exists(databaseName)) {
+          res.status(404).send("database '" + databaseName + "' doesn't exist");
+        }
+        else {
+          var password = req.jwt.password;
+          keepass.getDatabaseGroups(databaseName, password).then(function (result) {
+            res.json(result);
+          }, function (reason) {
+            res.status(500).send("problem occurred reading '" + databaseName + "': " + reason);
+          });
+        }
+      });
   app.post('/:filename/:group', function (req, res) {
     var databaseName = req.params.filename;
     if (!keepass.exists(databaseName)) {
-      res.send("database '" + databaseName + "' doesn't exist", 404);
+      res.status(404).send("database '" + databaseName + "' doesn't exist");
     }
     else {
       var password = req.body.password;
@@ -80,7 +106,7 @@
       keepass.getGroupEntries(databaseName, password, groupId).then(function (result) {
         res.json(result);
       }, function (reason) {
-        res.send("problem occurred reading '" + groupId + "' from '" + databaseName + "': " + reason, 500);
+        res.status(500).send("problem occurred reading '" + groupId + "' from '" + databaseName + "': " + reason);
       });
     }
   });
