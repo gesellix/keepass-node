d9672cddf7eedd600a0003d5e2ae347347532f0c
Tobias Gesellchen <tobias@gesellix.de>
2014-11-03 19:19:57 +0100

    combine backend and frontend coverage

--- karma.conf.js
+++ karma.conf.js
@@ -31,11 +31,18 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+          'js/app/**/*.js': 'coverage'
+        },
 
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress'],
+
+        coverageReporter: {
+          type: 'lcov',
+          dir: '../coverage-frontend/'
+        },
 
         // web server port
         port: 9876,
