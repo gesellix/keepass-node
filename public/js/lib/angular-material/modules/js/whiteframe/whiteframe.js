a13c91f3641d98dbeea843d02b9d9a786cc13efd
Tobias Gesellchen <tobias@gesellix.de>
2014-11-21 21:52:39 +0100

    update to angular-material 0.6.0-rc1-master-57f10f7

---
+++ public/js/lib/angular-material/modules/js/whiteframe/whiteframe.js
@@ -0,0 +1,15 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.6.0-rc1-master-57f10f7
+ */
+(function() {
+'use strict';
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+})();
