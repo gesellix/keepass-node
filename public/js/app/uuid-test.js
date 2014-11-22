dac5a758b427624924b75f55dfcd5a4b79d5e26b
Tobias Gesellchen <tobias@gesellix.de>
2014-11-22 11:03:32 +0100

    cleanup

--- public/js/app/uuid-test.js
+++ public/js/app/uuid-test.js
@@ -9,17 +9,12 @@
 
       inject(function (_uuid_) {
         uuid = _uuid_;
-        //$scope = $rootScope.$new();
-        //ctrl = $controller("NameOfYourController", {
-        //  $scope: $scope
-        //});
       });
     });
 
-    it('should create 32 digit hexadecimal string', function (done) {
+    it('should create 32 digit hexadecimal string', function () {
       var newUuid = uuid.create();
       expect(newUuid).to.match(/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/);
-      done();
     });
   });
 }());
